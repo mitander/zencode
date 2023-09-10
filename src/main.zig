e15060c40778de633e4942dff9d1c9a2b1ffbfc8
mitander <carlmitander@gmail.com>
2023-09-10 22:41:10 +0200

    Clean up

--- src/main.zig
+++ src/main.zig
@@ -1,25 +1,37 @@
 const std = @import("std");
 
-pub fn parse_bytes(raw_ben: []const u8, ally: std.mem.Allocator) !ValueTree {
-    var b = std.io.fixedBufferStream(raw_ben);
-    return parse_reader(b.reader(), ally);
-}
+const Map = std.StringArrayHashMapUnmanaged(Value);
+const ArrayList = std.ArrayList(Value);
 
-pub fn parse_reader(r: anytype, ally: std.mem.Allocator) !ValueTree {
-    return try ValueTree.parse(r, ally);
-}
+pub const ValueTree = struct {
+    arena: std.heap.ArenaAllocator,
+    root: Value,
+
+    pub fn parse(b: []const u8, ally: std.mem.Allocator) !ValueTree {
+        var buf = std.io.fixedBufferStream(b);
+        return try parse_reader(buf.reader(), ally);
+    }
+
+    pub fn parse_reader(reader: anytype, ally: std.mem.Allocator) !ValueTree {
+        var r: BencodeReader(@TypeOf(reader)) = .{ .child_reader = reader };
+        var arena = std.heap.ArenaAllocator.init(ally);
+        errdefer arena.deinit();
+        var values = try r.parse_inner(arena.allocator());
+        return ValueTree{ .arena = arena, .root = values };
+    }
+
+    pub fn deinit(self: *const ValueTree) void {
+        self.arena.deinit();
+    }
+};
 
 pub const Value = union(enum) {
     String: []const u8,
     Integer: i64,
     List: []const Value,
-    Dictionary: std.StringArrayHashMapUnmanaged(Value),
+    Dictionary: Map,
 
     const Self = @This();
-
-    pub fn deinit(self: Self, ally: std.mem.Allocator) void {
-        ally.free(self);
-    }
 
     pub fn format(self: Self, comptime fmt: []const u8, options: std.fmt.FormatOptions, writer: anytype) @TypeOf(writer).Error!void {
         _ = fmt;
@@ -82,30 +94,30 @@ pub const Value = union(enum) {
         }
     }
 
-    pub fn getDict(self: Self, key: []const u8) ?Value {
+    pub fn get_dict(self: Self, key: []const u8) ?Value {
         return self.lookup(key, .Dictionary);
     }
 
-    pub fn getList(self: Self, key: []const u8) ?[]const Value {
+    pub fn get_list(self: Self, key: []const u8) ?[]const Value {
         return self.lookup(key, .List);
     }
 
-    pub fn getString(self: Self, key: []const u8) ?[]const u8 {
+    pub fn get_string(self: Self, key: []const u8) ?[]const u8 {
         return self.lookup(key, .String);
     }
 
-    pub fn getInteger(self: Self, key: []const u8) ?i64 {
+    pub fn get_i64(self: Self, key: []const u8) ?i64 {
         return self.lookup(key, .Integer);
     }
 
-    pub fn getUnsignedInteger(self: Self, key: []const u8) ?u64 {
-        return @as(u64, self.getInteger(key)) orelse return null;
+    pub fn get_u64(self: Self, key: []const u8) ?u64 {
+        return @as(u64, self.get_i64(key)) orelse return null;
     }
 
     fn lookup(self: Self, key: []const u8, comptime tag: std.meta.FieldEnum(Value)) ?std.meta.FieldType(Value, tag) {
         std.debug.assert(self == .Dictionary);
-        const ret = self.Dictionary.get(key) orelse return null;
-        return if (ret == tag) @field(ret, @tagName(tag)) else null;
+        const val = self.Dictionary.get(key) orelse return null;
+        return if (val == tag) @field(val, @tagName(tag)) else null;
     }
 };
 
@@ -142,64 +154,64 @@ fn BencodeReader(comptime T: type) type {
             return self.buf;
         }
 
-        fn bytes(self: *Self, ally: std.mem.Allocator) ![]const u8 {
+        fn parse_bytes(self: *Self, ally: std.mem.Allocator) ![]const u8 {
             const b = try self.reader().readUntilDelimiterAlloc(ally, ':', 25);
             const len = try std.fmt.parseInt(usize, b, 10);
             var buf = try ally.alloc(u8, len);
-            const l = try self.reader().readAll(buf);
-            return buf[0..l];
+            _ = try self.reader().readAll(buf);
+            return buf;
         }
 
-        fn integer(self: *Self, ally: std.mem.Allocator) !i64 {
+        fn parse_integer(self: *Self, ally: std.mem.Allocator) !i64 {
             const b = try self.reader().readUntilDelimiterAlloc(ally, 'e', 25);
             return try std.fmt.parseInt(i64, b, 10);
         }
 
-        fn list(self: *Self, ally: std.mem.Allocator) ![]Value {
-            var arr = std.ArrayList(Value).init(ally);
+        fn parse_list(self: *Self, ally: std.mem.Allocator) ![]Value {
+            var arr = ArrayList.init(ally);
             while (try self.peek()) |c| {
                 if (c == 'e') {
                     self.buf = null;
                     return arr.toOwnedSlice();
                 }
-                const v = try self.inner(ally);
+                const v = try self.parse_inner(ally);
                 try arr.append(v);
             }
             return error.EndOfStream;
         }
 
-        fn dict(self: *Self, ally: std.mem.Allocator) !std.StringArrayHashMapUnmanaged(Value) {
-            var map = std.StringArrayHashMapUnmanaged(Value){};
-            while (try self.peek()) |c| {
-                if (c == 'e') {
+        fn parse_dict(self: *Self, ally: std.mem.Allocator) !Map {
+            var map = Map{};
+            while (try self.peek()) |char| {
+                if (char == 'e') {
                     self.buf = null;
                     return map;
                 }
-                const k = try self.bytes(ally);
-                const v = try self.inner(ally);
+                const k = try self.parse_bytes(ally);
+                const v = try self.parse_inner(ally);
                 try map.put(ally, k, v);
             }
             return error.EndOfStream;
         }
 
-        fn inner(self: *Self, ally: std.mem.Allocator) anyerror!Value {
+        fn parse_inner(self: *Self, ally: std.mem.Allocator) anyerror!Value {
             const char = try self.peek() orelse return error.EndOfStream;
 
             if (char >= '0' and char <= '9') {
                 return Value{
-                    .String = try self.bytes(ally),
+                    .String = try self.parse_bytes(ally),
                 };
             }
 
             switch (try self.reader().readByte()) {
                 'i' => return .{
-                    .Integer = try self.integer(ally),
+                    .Integer = try self.parse_integer(ally),
                 },
                 'l' => return .{
-                    .List = try self.list(ally),
+                    .List = try self.parse_list(ally),
                 },
                 'd' => return .{
-                    .Dictionary = try self.dict(ally),
+                    .Dictionary = try self.parse_dict(ally),
                 },
                 else => return error.BencodeBadDelimiter,
             }
@@ -207,38 +219,21 @@ fn BencodeReader(comptime T: type) type {
     };
 }
 
-pub const ValueTree = struct {
-    arena: std.heap.ArenaAllocator,
-    root: Value,
-
-    pub fn deinit(self: *const ValueTree) void {
-        self.arena.deinit();
-    }
-
-    pub fn parse(reader: anytype, allocator: std.mem.Allocator) !ValueTree {
-        var pr: BencodeReader(@TypeOf(reader)) = .{ .child_reader = reader };
-        var arena = std.heap.ArenaAllocator.init(allocator);
-        errdefer arena.deinit();
-        var value = try pr.inner(arena.allocator());
-        return ValueTree{ .arena = arena, .root = value };
-    }
-};
-
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     const ally = gpa.allocator();
     defer _ = gpa.deinit();
 
-    var f = try std.fs.cwd().openFile("./assets/example.torrent", .{});
-    defer f.close();
+    var file = try std.fs.cwd().openFile("./assets/example.torrent", .{});
+    defer file.close();
 
-    const v = try parse_reader(f.reader(), ally);
-    defer v.deinit();
+    const tree = try ValueTree.parse_reader(file.reader(), ally);
+    defer tree.deinit();
 
-    std.log.debug("{s}", .{v.root.getString("announce").?});
-    std.log.debug("{s}", .{v.root.getString("comment").?});
-    std.log.debug("{s}", .{v.root.getString("created by").?});
-    std.log.debug("{d}", .{v.root.getInteger("creation date").?});
+    std.log.debug("{s}", .{tree.root.get_string("announce").?});
+    std.log.debug("{s}", .{tree.root.get_string("comment").?});
+    std.log.debug("{s}", .{tree.root.get_string("created by").?});
+    std.log.debug("{d}", .{tree.root.get_i64("creation date").?});
 
-    try v.root.encode(std.io.getStdOut().writer());
+    try tree.root.encode(std.io.getStdOut().writer());
 }
